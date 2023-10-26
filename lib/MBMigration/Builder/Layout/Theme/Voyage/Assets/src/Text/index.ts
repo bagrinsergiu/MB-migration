afead0624067de36f0c0a75c777b0dcbbba1d2b9
Maxim Valuta <maxval1991@gmail.com>
2023-10-26 14:44:47 +0300

    fix(Assets): return value from esbuild

--- lib/MBMigration/Builder/Layout/Theme/Voyage/Assets/src/Text/index.ts
+++ lib/MBMigration/Builder/Layout/Theme/Voyage/Assets/src/Text/index.ts
@@ -2,5 +2,6 @@ import { getText } from "elements/src/Text";
 import { getData} from "elements/src/utils/getData";
 
 const data = getData();
+const output = getText(data);
 
-getText(data);
+export default output;
