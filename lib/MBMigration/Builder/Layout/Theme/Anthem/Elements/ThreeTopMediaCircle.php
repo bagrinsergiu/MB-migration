4caed93328f847cc8d9388f303d6e86f9af2820a
GolovencoSerghei <xreeny@gmail.com>
2024-01-31 14:38:04 +0200

    Refactor ThreeTopMediaCircle layout and remove unnecessary spacer
    The previous layout of the ThreeTopMediaCircle has been simplified and unnecessary spacer within the elements has been removed. Sheer modifications were also made to the "Item" and "Wrapper"'s structure. This refactoring could facilitate the maintenance and readability of the code.

--- lib/MBMigration/Builder/Layout/Theme/Anthem/Elements/ThreeTopMediaCircle.php
+++ lib/MBMigration/Builder/Layout/Theme/Anthem/Elements/ThreeTopMediaCircle.php
@@ -75,7 +75,10 @@ class ThreeTopMediaCircle extends Element
                 $objItem->item(0)->item(0)->setting('imageSrc', '');
                 $objItem->item(0)->item(0)->setting('imageFileName', '');
             }
+        }
 
+        foreach ($sectionData['items'] as $item)
+        {
             if ($item['item_type'] === 'title') {
 
                 $this->textCreation($item, $objBlock);
@@ -83,7 +86,11 @@ class ThreeTopMediaCircle extends Element
 //                $richText = JS::RichText($item['id'], $options['currentPageURL'], $options['fontsFamily']);
 //                $objBlock->item(0)->item(1)->item(0)->item(0)->item(0)->setText($richText);
             }
+        }
 
+
+        foreach ($sectionData['items'] as $item)
+        {
             if ($item['item_type'] === 'body') {
 
                 $this->textCreation($item, $objBlock);
@@ -92,7 +99,9 @@ class ThreeTopMediaCircle extends Element
 //                $objBlock->item(0)->item(1)->item(0)->item(2)->item(0)->setText($richText);
             }
         }
-        $objBlock->item(0)->item(0)->addItem($objSpacer->get());
+
+
+//        $objBlock->item(0)->item(0)->addItem($objSpacer->get());
 
         $block = $this->replaceIdWithRandom($objBlock->get());
         return json_encode($block);
@@ -110,7 +119,7 @@ class ThreeTopMediaCircle extends Element
                     if(!empty($sectionData['content'])) {
                         $embedCode = $this->findEmbeddedPasteDivs($sectionData['content']);
                         if(is_array($embedCode)){
-                            $objBlock->item(0)->item(1)->item(0)->item(0)->addItem($this->embedCode($embedCode[$i]));
+                            $objBlock->item()->item(1)->item()->addItem($this->embedCode($embedCode[$i]));
                         }
                         $i++;
                     }
@@ -121,10 +130,10 @@ class ThreeTopMediaCircle extends Element
                             $iconItem['value']['borderStyle'] = "none";
                         }
                     }
-                    $objBlock->item(0)->item(1)->item(0)->item(0)->addItem($textItem);
+                    $objBlock->item()->item(1)->item()->addItem($textItem);
                     break;
                 case 'Wrapper':
-                    $objBlock->item(0)->item(1)->item(0)->item(0)->addItem($textItem);
+                    $objBlock->item()->item(1)->item()->addItem($textItem);
                     break;
             }
         }
