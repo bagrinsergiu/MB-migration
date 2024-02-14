324582b07c6241ce750edca1a2d4919a2469444e
Maxim Valuta <maxval1991@gmail.com>
2024-02-14 08:52:10 +0200

    fix(anthem): getText

--- lib/MBMigration/Builder/Layout/Theme/Voyage/Assets/src/index.ts
+++ lib/MBMigration/Builder/Layout/Theme/Voyage/Assets/src/index.ts
@@ -1,11 +1,11 @@
-import { globalMenuExtractor } from "./GlobalMenu";
-import { globalExtractor } from "./Globals";
+import { run as globalMenuExtractor } from "./GlobalMenu";
 import { run as getMenu } from "./Menu";
 import { run as getStyles } from "./StyleExtractor";
+import { run as getText } from "./Text";
 
 window.brizy = {
   globalMenuExtractor,
-  globalExtractor,
   getMenu,
-  getStyles
+  getStyles,
+  getText
 };
