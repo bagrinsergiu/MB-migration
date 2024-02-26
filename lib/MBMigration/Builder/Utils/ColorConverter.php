e4a773765894038f6d6960c3847bb193184bd461
SergheiGolovecno <xreeny@gmail.com>
2024-02-26 17:21:37 +0200

    Update theme to use BrowserPHP and ColorConverter
    Replaced the deprecated Browser and manually implemented color conversion functionality with BrowserPHP and ColorConverter respectively. This change increases the efficiency of the Anthem theme by enabling the use of PHP features for Browser operations and providing a utility for color conversion. It also offers better maintainability by removing the hard-coded convertColor function.

--- lib/MBMigration/Builder/Utils/ColorConverter.php
+++ lib/MBMigration/Builder/Utils/ColorConverter.php
@@ -56,4 +56,72 @@ final class ColorConverter
             return 1;
         }
     }
+
+    public static function convertColor($color)
+    {
+
+        if (preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $color)) {
+            return $color;
+        }
+
+        if (preg_match('/rgba\((\d+), (\d+), (\d+), ([0-9]*\.?[0-9]+)\)/', $color, $matches)) {
+            $r = $matches[1];
+            $g = $matches[2];
+            $b = $matches[3];
+            $a = $matches[4];
+
+            $color = sprintf("#%02X%02X%02X", $r, $g, $b);
+
+            if ($a == 0 && $color === "#000000") {
+                return '#ffffff';
+            } else {
+                return [
+                    'color' => sprintf("#%02X%02X%02X", $r, $g, $b),
+                    'opacity' => $a,
+                ];
+            }
+        }
+
+        if (preg_match_all("/rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/", $color, $matches)) {
+
+            list($r, $g, $b) = array($matches[1][0], $matches[2][0], $matches[3][0]);
+
+            return sprintf("#%02X%02X%02X", $r, $g, $b);
+        }
+
+        return $color;
+    }
+
+   public static function convertStyle($sectionStyles, &$style, $section = 'data')
+    {
+        if (isset($sectionStyles['data'])) {
+            $opacityIsSet = false;
+            foreach ($sectionStyles[$section] as $key => $value) {
+                $data = (new ColorConverter)->removePx($value);
+                $convertedData = ColorConverter::convertColor(str_replace("px", "", $value));
+                if (is_array($convertedData)) {
+                    $style[$key] = $convertedData['color'];
+                    $style['opacity'] = $convertedData['opacity'];
+                    $opacityIsSet = true;
+                } else {
+                    if ($opacityIsSet && $key == 'opacity') {
+                        continue;
+                    } else {
+                        $style[$key] = $convertedData;
+                    }
+                }
+            }
+        }
+    }
+
+    private function removePx($inputString)
+    {
+        $pos = strpos($inputString, "px");
+
+        if ($pos !== false) {
+            $inputString = substr_replace($inputString, "", $pos, 2);
+        }
+
+        return $inputString;
+    }
 }
