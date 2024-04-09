88f7c3beaeeba4e3642daa52c29c6417ec13e481
Corina Suletea <corinasulete@gmail.com>
2024-04-09 10:54:35 +0300

    feat: removed all empty p tags that do not include br

--- packages/elements/src/Text/utils/dom/removeEmptyNodes.ts
+++ packages/elements/src/Text/utils/dom/removeEmptyNodes.ts
@@ -1,5 +1,17 @@
 export function removeEmptyNodes(node: Element): Element {
-  node.innerHTML = node.innerHTML.replace(/\n\s*/g, " ");
+  const children = Array.from(node.children);
+
+  children.forEach((child) => {
+    const text = child.textContent;
+
+    // Check if have only `\n` then remove it
+    // when have <br> textContent is empty string ['']
+    if (text && text.includes("\n") && !text.trim()) {
+      child.remove();
+    }
+  });
+
+  node.innerHTML = node.innerHTML.replace(/\n/g, " ");
 
   return node;
 }
