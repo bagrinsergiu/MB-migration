f6252b98c480d11173de36fbad68dc610c91f962
CristianSpatari <spataricristinel@gmail.com>
2024-04-19 16:12:59 +0300

    feat(attributes): extractor

--- lib/MBMigration/Builder/Layout/Theme/Voyage/Assets/src/index.ts
+++ lib/MBMigration/Builder/Layout/Theme/Voyage/Assets/src/index.ts
@@ -1,10 +1,14 @@
-import { run as getStyles } from "./StyleExtractor";
-import { run as getText } from "./Text";
 import { getMenuItem, getSubMenuItem } from "./Menu";
+import {
+  attributeRun as getAttributes,
+  run as getStyles
+} from "./StyleExtractor";
+import { run as getText } from "./Text";
 
 window.brizy = {
   getMenuItem,
   getSubMenuItem,
+  getAttributes,
   getStyles,
   getText
 };
