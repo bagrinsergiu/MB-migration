521609215410a2de7306b41e2aa6f052b19c0cbd
SergheiGolovecno <xreeny@gmail.com>
2024-05-08 14:45:28 +0300

    Add .editorconfig and adjust indentations in getRootPropertyStyles
    This commit introduces a new .editorconfig file to enforce consistent coding style. It sets the general indent style to spaces with an indent size of 2, except for PHP files which have an indent size of 4. Additionally, the indentations in getRootPropertyStyles.ts file have been adjusted to comply with these settings.

--- packages/elements/src/Dom/getRootPropertyStyles.ts
+++ packages/elements/src/Dom/getRootPropertyStyles.ts
@@ -1,31 +1,31 @@
-import { Output } from "../types/type";
-import { createData } from "../utils/getData";
+import {Output} from "../types/type";
+import {createData} from "../utils/getData";
 
 export const getRootPropertyStyles = (): Output => {
-    const data = {};
-    const styleSheets = document.styleSheets;
+  const data = {};
+  const styleSheets = document.styleSheets;
 
-    for (let i = 0; i < styleSheets.length; i++) {
-        const styleSheet = styleSheets[i];
+  for (let i = 0; i < styleSheets.length; i++) {
+    const styleSheet = styleSheets[i];
 
-        if (!styleSheet.href) {
-            const cssRules = styleSheet.cssRules || styleSheet.rules;
+    if (!styleSheet.href) {
+      const cssRules = styleSheet.cssRules || styleSheet.rules;
 
-            for (let j = 0; j < cssRules.length; j++) {
-                const rule = cssRules[j];
+      for (let j = 0; j < cssRules.length; j++) {
+        const rule = cssRules[j];
 
-                if (rule.selectorText === ":root") {
-                    const declarations = rule.style;
+        if (rule.selectorText === ":root") {
+          const declarations = rule.style;
 
-                    for (let k = 0; k < declarations.length; k++) {
-                        const property = declarations[k];
-                        const value = declarations.getPropertyValue(property);
-                        data[property] = value;
-                    }
-                }
-            }
+          for (let k = 0; k < declarations.length; k++) {
+            const property = declarations[k];
+            const value = declarations.getPropertyValue(property);
+            data[property] = value;
+          }
         }
+      }
     }
+  }
 
-    return createData({ data });
+  return createData({data});
 };
