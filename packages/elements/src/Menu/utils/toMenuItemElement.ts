e54fb616fb2f18aa2fccafdc8493a39b8b45f2f0
zeleniucvladislav <zeleniuc.vladislav@gmail.com>
2024-08-06 17:49:16 +0300

    fix(Menu): menu hover and mobile menu icon color

---
+++ packages/elements/src/Menu/utils/toMenuItemElement.ts
@@ -0,0 +1,33 @@
+import { MenuItemSelector } from "types/type";
+
+interface ToMenuItemElement {
+  node: Element | null;
+  selector?: MenuItemSelector;
+  targetSelector: string;
+}
+
+interface ToMenuItemElementRes {
+  item: Element;
+  pseudoEl: string;
+}
+
+export const toMenuItemElement = ({
+  node,
+  selector,
+  targetSelector
+}: ToMenuItemElement): ToMenuItemElementRes | undefined => {
+  if (!selector || !node) {
+    return;
+  }
+
+  const targetElement = node.querySelector(targetSelector);
+
+  if (targetElement) {
+    return {
+      item: targetElement,
+      pseudoEl: selector.pseudoEl
+    };
+  }
+
+  return;
+};
