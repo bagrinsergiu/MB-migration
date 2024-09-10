90538325864b39361c02955ca1fa00ac09287792
SergheiGolovecno <xreeny@gmail.com>
2024-09-10 15:41:14 +0300

    Comment out handling of excess menu items.
    Temporarily disable the logic that handles additional menu items beyond the main menu item count in both Bloom and Voyage MenuBuilder. This change retains the menu item filtering for hidden items while preserving the current menu structure.

--- lib/MBMigration/Builder/Layout/Theme/Voyage/MenuBuilder.php
+++ lib/MBMigration/Builder/Layout/Theme/Voyage/MenuBuilder.php
@@ -12,33 +12,33 @@ class MenuBuilder extends \MBMigration\Builder\Layout\Common\MenuBuilder
         // filter hidden menu items
         $menuItems = array_values(array_filter($menuItems, fn($item) => isset($item['hidden']) && !$item['hidden']));
 
-        $count = count($menuItems);
-        if ($count > self::MAIN_MENU_ITEM_MAX_COUNT) {
-            $moreItem = [
-                'id' => null,
-                'slug' => "",
-                'name' => self::MORE_MENU_ITEM_LABEL,
-                'parent_id' => null,
-                'collection' => null,
-                'position' => 7,
-                'landing' => true,
-                'hidden' => false,
-                'parentSettings' => "{}",
-                'protectedPage' => false,
-                'child' => [],
-                'iconName'=>'circle-down-40',
-                'iconType'=>'outline'
-            ];
-
-            for ($i = self::MAIN_MENU_ITEM_MAX_COUNT; $i < $count; $i++) {
-                // remove child fot the more items
-                //$menuItems[$i]['child'] = [];
-                $moreItem['child'][] = $menuItems[$i];
-                unset($menuItems[$i]);
-            }
-            // add more item
-            $menuItems[] = $moreItem;
-        }
+//        $count = count($menuItems);
+//        if ($count > self::MAIN_MENU_ITEM_MAX_COUNT) {
+//            $moreItem = [
+//                'id' => null,
+//                'slug' => "",
+//                'name' => self::MORE_MENU_ITEM_LABEL,
+//                'parent_id' => null,
+//                'collection' => null,
+//                'position' => 7,
+//                'landing' => true,
+//                'hidden' => false,
+//                'parentSettings' => "{}",
+//                'protectedPage' => false,
+//                'child' => [],
+//                'iconName'=>'circle-down-40',
+//                'iconType'=>'outline'
+//            ];
+//
+//            for ($i = self::MAIN_MENU_ITEM_MAX_COUNT; $i < $count; $i++) {
+//                // remove child fot the more items
+//                //$menuItems[$i]['child'] = [];
+//                $moreItem['child'][] = $menuItems[$i];
+//                unset($menuItems[$i]);
+//            }
+//            // add more item
+//            $menuItems[] = $moreItem;
+//        }
 
         return parent::transformToBrizyMenu(array_values($menuItems));
     }
