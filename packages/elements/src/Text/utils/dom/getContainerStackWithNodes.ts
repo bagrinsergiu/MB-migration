2e7187258686aef338f578caa0a35bd108ba5ea5
zeleniucvladislav <zeleniuc.vladislav@gmail.com>
2024-10-24 16:39:34 +0300

    fix(richtext): wrong icon aligment

--- packages/elements/src/Text/utils/dom/getContainerStackWithNodes.ts
+++ packages/elements/src/Text/utils/dom/getContainerStackWithNodes.ts
@@ -69,7 +69,7 @@ const extractInnerText = (node: Node, stack: Stack, selector: string): void => {
   }
 };
 
-function appendNodeStyles(node: HTMLElement, targetNode: HTMLElement) {
+function appendNodeStyles(node: HTMLElement, targetNode: HTMLElement = node) {
   const styles = window.getComputedStyle(node);
   extractedAttributes.forEach((style) => {
     targetNode.style.setProperty(style, styles.getPropertyValue(style));
@@ -96,7 +96,7 @@ function removeNestedDivs(node: HTMLElement) {
         // insert granchild to child parent node and remove child
         Array.from(child.childNodes).forEach((grandchild) => {
           if (grandchild instanceof HTMLElement) {
-            appendNodeStyles(grandchild, grandchild);
+            appendNodeStyles(grandchild);
 
             node.insertBefore(grandchild, child);
           } else if (grandchild.textContent?.trim()) {
@@ -209,7 +209,7 @@ export const getContainerStackWithNodes = (parentNode: Element): Container => {
               if (node instanceof HTMLElement) {
                 const container = document.createElement("div");
                 container.append(node.cloneNode(true));
-                appendNodeStyles(node, node);
+                appendNodeStyles(node);
 
                 // if latest appended is icon, icons must be wrapped in same node
                 if (appendedButton) {
@@ -236,6 +236,8 @@ export const getContainerStackWithNodes = (parentNode: Element): Container => {
               container.append(node.cloneNode(true));
 
               if (container.querySelector(iconSelector)) {
+                parentNode.parentElement?.append(_node);
+                appendNodeStyles(node);
                 // if latest appended is icon, icons must be wrapped in same node
                 if (appendedIcon) {
                   stack.set(node);
@@ -259,6 +261,7 @@ export const getContainerStackWithNodes = (parentNode: Element): Container => {
               }
             }
           });
+          _node.remove();
           return;
         }
       }
