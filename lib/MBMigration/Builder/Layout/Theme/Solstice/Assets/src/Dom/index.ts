7f48c431d1fdc4901fa92f5d04d282159b7eb6e0
zeleniucvladislav <zeleniuc.vladislav@gmail.com>
2024-10-31 15:33:01 +0200

    feat(extractors): added node class add and remove functions

--- lib/MBMigration/Builder/Layout/Theme/Solstice/Assets/src/Dom/index.ts
+++ lib/MBMigration/Builder/Layout/Theme/Solstice/Assets/src/Dom/index.ts
@@ -1,11 +1,15 @@
+import { addNodeClass } from "elements/src/Dom/addNodeClass";
 import { detectSubpalette } from "elements/src/Dom/detectSubpalette";
 import { getNodeText } from "elements/src/Dom/getNodeText";
 import { getRootPropertyStyles } from "elements/src/Dom/getRootPropertyStyles";
 import { hasNode } from "elements/src/Dom/hasNode";
+import { removeNodeClass } from "elements/src/Dom/removeNodeClass";
 
 export const dom = {
   hasNode,
   getNodeText,
   getRootPropertyStyles,
-  detectSubpalette
+  detectSubpalette,
+  addNodeClass,
+  removeNodeClass
 };
