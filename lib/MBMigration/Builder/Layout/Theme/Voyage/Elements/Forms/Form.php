1e3ee5f3e4068a8b20f0547ac4cfa67d4b951be0
serghei <golovenco.serghei@gmail.com>
2024-11-13 13:57:12 +0200

    Add form handling and new form elements
    Implemented form handling logic in `FullWidthForm` and `LeftForm` by associating forms with unique IDs. Added a new `Form` class to manage form properties and paddings, and updated global blocks configuration for sermon details and layout.

--- lib/MBMigration/Builder/Layout/Theme/Voyage/Elements/Forms/Form.php
+++ lib/MBMigration/Builder/Layout/Theme/Voyage/Elements/Forms/Form.php
@@ -1,41 +1,27 @@
 <?php
 
-namespace MBMigration\Builder\Layout\Theme\Anthem\Elements\Forms;
+namespace MBMigration\Builder\Layout\Theme\Voyage\Elements\Forms;
 
-use MBMigration\Browser\BrowserPageInterface;
 use MBMigration\Builder\BrizyComponent\BrizyComponent;
 use MBMigration\Builder\Layout\Common\Elements\Forms\FormElement;
-use MBMigration\Builder\Layout\Common\ElementContextInterface;
 
-class FullWidthForm extends FormElement
+
+class Form extends FormElement
 {
     protected function getJsonFromBrizyKit()
     {
         return $this->brizyKit['full-width'];
     }
 
-    protected function getFormContainerElement(BrizyComponent $brizyComponent): BrizyComponent {
+    protected function getFormContainerElement(BrizyComponent $brizyComponent): BrizyComponent
+    {
         return $brizyComponent->getItemWithDepth(0);
-    }
-
-    protected function handleForm(ElementContextInterface $elementContext, BrowserPageInterface $browserPage): BrizyComponent {
-        // add the form here.
-        $mbSection = $elementContext->getMbSection();
-        $formId = $mbSection['settings']['sections']['form']['form_id'] ?? '';
-
-        $form = new BrizyComponent(json_decode($this->brizyKit['form-wrapper'], true));
-        $form->getItemWithDepth(0)->getValue()->set_form($formId);
-
-        $brizyComponent = $elementContext->getBrizySection();
-        $brizyComponent->getValue()->set_items([$form]);
-
-        return $brizyComponent;
     }
 
     protected function getPropertiesMainSection(): array
     {
         return [
-            "mobilePaddingType"=> "ungrouped",
+            "mobilePaddingType" => "ungrouped",
             "mobilePadding" => 0,
             "mobilePaddingSuffix" => "px",
             "mobilePaddingTop" => 50,
