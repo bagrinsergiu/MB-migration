8320e4688dd8dba7736685fffd01b150267a9bbc
Nichita <nichita@brizy.io>
2025-04-23 09:56:45 +0300

    Reduce top and bottom padding in LeftMedia and RightMedia.

--- lib/MBMigration/Builder/Layout/Theme/Voyage/Elements/Text/LeftMedia.php
+++ lib/MBMigration/Builder/Layout/Theme/Voyage/Elements/Text/LeftMedia.php
@@ -59,9 +59,9 @@ class LeftMedia extends PhotoTextElement
             "mobilePaddingLeftSuffix" => "px",
 
             "paddingType" => "ungrouped",
-            "paddingTop" => 100,
+            "paddingTop" => 0,
             "paddingTopSuffix" => "px",
-            "paddingBottom" => 100,
+            "paddingBottom" => 0,
             "paddingBottomSuffix" => "px",
             "paddingRight" => 0,
             "paddingRightSuffix" => "px",
