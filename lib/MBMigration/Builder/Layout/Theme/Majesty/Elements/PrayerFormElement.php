b53ebfe2b531b1161e8a6ddcb38b5fa05475a533
golovencoserghei <golovenco.serghei@gmail.com>
2025-05-21 15:33:34 +0300

    refactor: reorganize namespace structure for Majesty elements
    Updated namespace paths to better align elements under relevant categories such as Text, Forms, Events, and others. This change improves code organization, readability, and maintainability by grouping interrelated components logically.

--- lib/MBMigration/Builder/Layout/Theme/Majesty/Elements/PrayerFormElement.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace MBMigration\Builder\Layout\Theme\Majesty\Elements;
-
-class PrayerFormElement extends \MBMigration\Builder\Layout\Common\Elements\Prayer\PrayerFormElement
-{
-
-}
---
+++ lib/MBMigration/Builder/Layout/Theme/Majesty/Elements/Sermons/GridMediaLayout.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MBMigration\Builder\Layout\Theme\Majesty\Elements\Sermons;
+
+use MBMigration\Builder\Layout\Theme\Ember\Elements\Sermons\AbstractMediaLayout;
+
+class GridMediaLayout extends AbstractMediaLayout
+{
+
+}
