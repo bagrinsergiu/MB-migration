45552a2f939f4d12998236255c8b05348ffd3868
golovencoserghei <golovenco.serghei@gmail.com>
2025-07-11 15:07:54 +0300

    feat: improved page handling
    Introduced the `launch_bld` method to handle pages based on menu items and their associated settings. This enhances the flexibility of page processing during migrations. Also added a `mbPageId` field to the menu item structure for better tracking.

--- lib/MBMigration/Builder/Layout/Common/MenuBuilder.php
+++ lib/MBMigration/Builder/Layout/Common/MenuBuilder.php
@@ -86,6 +86,7 @@ class MenuBuilder implements MenuBuilderInterface
 
         $newItem = [
             "uid" => Utils::getNameHash(),
+            "mbPageId" => $item['id'],
             "isNewTab" => $this->checkOpenInNewTab($settings),
             'isIndex'=>$item['position']==1 && !$item['parent_id'] && $item['landing'],
             "label" => TextTools::transformText($item['name'], $textTransformMenu),
