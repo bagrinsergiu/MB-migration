ecab0d1f218c0acf1366748b6ea52d8c63eb5591
golovencoserghei <golovenco.serghei@gmail.com>
2025-09-17 16:45:15 +0300

    feat: add MiddleMedia block and enhance group handling
    Introduced the `MiddleMedia` block in the `Dusk` theme, including element definitions and registration in the `ElementFactory`. Added `getItemsByGroup` method in `MbSectionUtils` to improve item grouping and handling.

---
+++ lib/MBMigration/Builder/Layout/Theme/Dusk/Elements/Text/MiddleMedia.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MBMigration\Builder\Layout\Theme\Dusk\Elements\Text;
+
+use MBMigration\Builder\BrizyComponent\BrizyComponent;
+use MBMigration\Builder\Layout\Common\Elements\Text\MiddleMediaElement;
+
+class MiddleMedia extends MiddleMediaElement
+{
+    protected function getSectionItemComponent(BrizyComponent $brizySection): BrizyComponent
+    {
+        return $brizySection->getItemWithDepth(0);
+    }
+
+    protected function getTextContainerComponent(BrizyComponent $brizySection): BrizyComponent {
+        return $brizySection->getItemWithDepth(0,0,0);
+    }
+
+    protected function getTopPaddingOfTheFirstElement(): int
+    {
+        return 50;
+    }
+
+    protected function getMobileTopPaddingOfTheFirstElement(): int
+    {
+        return 25;
+    }
+
+}
