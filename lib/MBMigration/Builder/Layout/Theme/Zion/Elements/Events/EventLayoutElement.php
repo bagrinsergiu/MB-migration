4395c1a78e4363d4d9219d91f7b7e9cc00314765
Zaharia Alexandru <alecszaharia@gmail.com>
2025-10-30 11:44:12 +0200

    Refactor zion events styles

--- lib/MBMigration/Builder/Layout/Theme/Zion/Elements/Events/EventLayoutElement.php
+++ lib/MBMigration/Builder/Layout/Theme/Zion/Elements/Events/EventLayoutElement.php
@@ -2,6 +2,9 @@
 
 namespace MBMigration\Builder\Layout\Theme\Zion\Elements\Events;
 
+use MBMigration\Builder\Layout\Common\ElementContextInterface;
+use MBMigration\Builder\Utils\ColorConverter;
+
 class EventLayoutElement extends \MBMigration\Builder\Layout\Common\Elements\Events\EventLayoutElement
 {
     protected function getPropertiesMainSection(): array
@@ -21,160 +24,22 @@ class EventLayoutElement extends \MBMigration\Builder\Layout\Common\Elements\Eve
         ];
     }
 
-    protected function getStyleOptions($sectionPalette, $basicButtonStyleNormal, $basicButtonStyleHover, $fonts): array
+    protected function filterEventLayoutElementStyles($sectionProperties, ElementContextInterface $data): array
     {
-        return [
-            'eventDetailPageButtonText' => 'Learn More',
+        $mbSection = $data->getMbSection();
+        $selector = '[data-id="' . ($mbSection['sectionId'] ?? $mbSection['id']) . '"]';
+        $sectionSubPalette = $this->getNodeSubPalette($selector, $this->browserPage);
+        $sectionPalette = $data->getThemeContext()->getRootPalettes()->getSubPaletteByName($sectionSubPalette);
 
-            'titleTypographyLineHeight' => 1.8,
+        $basicButtonStyleNormal = $this->pageTDO->getButtonStyle()->getNormal();
+        $basicButtonStyleHover = $this->pageTDO->getButtonStyle()->getHover();
 
-            'listItemMetaTypographyLineHeight' => 1.8,
+        ColorConverter::rewriteColorIfSetOpacity($basicButtonStyleNormal);
+        ColorConverter::rewriteColorIfSetOpacity($basicButtonStyleHover);
 
-            'dateTypographyLineHeight' => 1.8,
-            'eventsTypographyLineHeight' => 1.8,
-
-            'dateTypographyFontStyle' => '',
-            'dateTypographyFontFamily' => 1.8,
-
-            'previewColorHex' => '#f8f8f8',
-            'previewColorOpacity' => 1,
-            'previewColorPalette' => '',
-
-            'hoverPreviewColorHex' => '#f8f8f8',
-            'hoverPreviewColorOpacity' => 0.8,
-            'hoverPreviewColorPalette' => '',
-
-            'resultsHeadingColorHex' => $sectionPalette['text'],
-            'resultsHeadingColorOpacity' => 1,
-            'resultsHeadingColorPalette' => '',
-
-            'resultsHeadingTypographyFontFamilyType' => $fonts,
-            'resultsHeadingTypographyFontStyle' => '',
-
-            'listPaginationArrowsColorHex' => $sectionPalette['text'],
-            'listPaginationArrowsColorOpacity' => 1,
-            'listPaginationArrowsColorPalette' => '',
-
-            'hoverListPaginationArrowsColorHex' => $sectionPalette['text'],
-            'hoverListPaginationArrowsColorOpacity' => 0.75,
-            'hoverListPaginationArrowsColorPalette' => '',
-
-            'listPaginationColorHex' => $sectionPalette['text'],
-            'listPaginationColorOpacity' => 1,
-            'listPaginationColorPalette' => '',
-
-            'calendarDaysBgColorHex' => $sectionPalette['bg'] ?? $basicButtonStyleNormal['background-color'],
-            'calendarDaysBgColorOpacity' => $basicButtonStyleNormal['background-color-opacity'] ?? 1,
-            'calendarDaysBgColorPalette' => '',
-
-            'calendarHeadingColorHex' => $sectionPalette['text'] ?? $basicButtonStyleNormal['color'],
-            'calendarHeadingColorOpacity' => $basicButtonStyleNormal['color-opacity'] ?? 1,
-            'calendarHeadingColorPalette' => '',
-
-            'calendarDaysColorHex' => $sectionPalette['text'] ?? $basicButtonStyleNormal['color'],
-            'calendarDaysColorOpacity' => $basicButtonStyleNormal['color-opacity'] ?? 1,
-            'calendarDaysColorPalette' => '',
-
-            'eventsColorHex' => $sectionPalette['link'],
-            'eventsColorOpacity' => 1,
-            'eventsColorPalette' => '',
-
-            'hoverEventsColorHex' => $sectionPalette['link'],
-            'hoverEventsColorOpacity' => 0.75,
-            'hoverEventsColorPalette' => '',
-
-            'listItemTitleColorHex' => $sectionPalette['link'],
-            'listItemTitleColorOpacity' => 1,
-            'listItemTitleColorPalette' => '',
-
-            'hoverListItemTitleColorHex' => $sectionPalette['link'],
-            'hoverListItemTitleColorOpacity' => 0.75,
-            'hoverListItemTitleColorPalette' => '',
-
-            'listItemMetaColorHex' => $sectionPalette['text'],
-            'listItemMetaColorOpacity' => 1,
-            'listItemMetaColorPalette' => '',
-
-            'listItemDateColorHex' => $sectionPalette['btn-text'] ?? $basicButtonStyleHover['color'] ?? $sectionPalette['text'],
-            'listItemDateColorOpacity' => 0.75 ?? $basicButtonStyleHover['color-opacity'] ?? 1,
-            'listItemDateColorPalette' => '',
-
-            'listTitleColorHex' => $sectionPalette['text'],
-            'listTitleColorOpacity' => 1,
-            'listTitleColorPalette' => '',
-
-            'groupingDateColorHex' => $sectionPalette['text'],
-            'groupingDateColorOpacity' => 1,
-            'groupingDateColorPalette' => '',
-
-            'hoverTitleColorHex' => $sectionPalette['link'],
-            'hoverTitleColorOpacity' => 0.75,
-            'hoverTitleColorPalette' => '',
-
-            'titleColorHex' => $sectionPalette['link'],
-            'titleColorOpacity' => 1,
-            'titleColorPalette' => '',
-
-            'dateColorHex' => $sectionPalette['text'],
-            'dateColorOpacity' => 1,
-            'dateColorPalette' => '',
-
-            'listItemDateBgColorHex' => $basicButtonStyleHover['background-color'] ?? $sectionPalette['btn-bg'] ?? $sectionPalette['btn'],
-            'listItemDateBgColorOpacity' => $basicButtonStyleHover['background-color-opacity'] ?? 1,
-            'listItemDateBgColorType' => 'solid',
-            'listItemDateBgColorPalette' => '',
-
-            'detailButtonBgColorHex' => $basicButtonStyleNormal['background-color'] ?? $sectionPalette['btn-bg'] ?? $sectionPalette['btn'],
-            'detailButtonBgColorOpacity' => $basicButtonStyleNormal['background-color-opacity'] ?? 1,
-            'detailButtonBgColorPalette' => '',
-
-            "hoverDetailButtonBgColorType" => "solid",
-            'hoverDetailButtonBgColorHex' => $basicButtonStyleHover['background-color'] ?? $sectionPalette['btn-bg'] ?? $sectionPalette['btn'],
-            'hoverDetailButtonBgColorOpacity' => $basicButtonStyleHover['background-color-opacity'] ?? 1,
-            'hoverDetailButtonBgColorPalette' => '',
-
-//            'detailButtonBorderStyle' => 'solid',
-//            'detailButtonBorderColorHex' => $basicButtonStyleNormal['border-top-color'] ?? $sectionPalette['btn-text'] ?? $sectionPalette['text'],
-//            'detailButtonBorderColorOpacity' => $basicButtonStyleNormal['border-top-color-opacity'] ?? 1,
-//            'detailButtonBorderColorPalette' => '',
-//
-//            "detailButtonBorderWidthType" => "grouped",
-//            "detailButtonBorderWidth" => 1,
-//            "detailButtonBorderTopWidth" => 1,
-//            "detailButtonBorderRightWidth" => 1,
-//            "detailButtonBorderBottomWidth" => 1,
-//            "detailButtonBorderLeftWidth" => 1,
-
-            'detailButtonColorHex' => $basicButtonStyleNormal['color'] ?? $sectionPalette['btn-text'] ?? $sectionPalette['text'],
-            'detailButtonColorOpacity' => 1,
-            'detailButtonColorPalette' => '',
-
-            'hoverDetailButtonColorHex' => $basicButtonStyleHover['color'] ?? $sectionPalette['btn-text'] ?? $sectionPalette['text'],
-            'hoverDetailButtonColorOpacity' => 0.75 ?? $basicButtonStyleHover['color-opacity'] ?? 1,
-            'hoverDetailButtonColorPalette' => '',
-
-            'detailButtonGradientColorHex' => $sectionPalette['btn-text'] ?? $sectionPalette['text'],
-            'detailButtonGradientColorOpacity' => 1,
-            'detailButtonGradientColorPalette' => '',
-
-            'hoverViewColorHex' => $sectionPalette['text'],
-            'hoverViewColorOpacity' => 1,
-            'hoverViewColorPalette' => '',
-
-            'viewColorHex' => $sectionPalette['text'],
-            'viewColorOpacity' => 0.7,
-            'viewColorPalette' => '',
-
-            'activeViewColorHex' => $sectionPalette['text'],
-            'activeViewColorOpacity' => 1,
-            'activeViewColorPalette' => '',
-
-            'layoutViewTypographyFontFamily' => $fonts,
-            'layoutViewTypographyFontStyle' => '',
-            'layoutViewTypographyFontFamilyType' => 'upload',
-
-            "detailButtonBorderStyle" => $basicButtonStyleNormal['border-style'] ?? "solid",
-            "detailButtonBorderColorHex" =>  $basicButtonStyleNormal['border-top-color'],
+        $customSectionProperties = [
+           "detailButtonBorderStyle" => $basicButtonStyleNormal['border-style'] ?? "solid",
+            "detailButtonBorderColorHex" => $basicButtonStyleNormal['border-top-color'],
             "detailButtonBorderColorOpacity" => $basicButtonStyleNormal['border-top-color-opacity'] ?? 1,
             "detailButtonBorderColorPalette" => "",
 
@@ -197,6 +62,7 @@ class EventLayoutElement extends \MBMigration\Builder\Layout\Common\Elements\Eve
             "hoverDetailButtonBorderBottomWidth" => $basicButtonStyleNormal['border-width'],
             "hoverDetailButtonBorderLeftWidth" => $basicButtonStyleNormal['border-width'],
         ];
+        return array_merge($sectionProperties, $customSectionProperties);
     }
 
     protected function getTopPaddingOfTheFirstElement(): int
