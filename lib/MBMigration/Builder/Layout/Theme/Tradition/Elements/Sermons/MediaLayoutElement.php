fd0362d27d9950603bce1a4523e41c1df02dac8b
Zaharia Alexandru <alecszaharia@gmail.com>
2025-11-04 08:54:20 +0200

    Fixed sermon widget wrong height

--- lib/MBMigration/Builder/Layout/Theme/Tradition/Elements/Sermons/MediaLayoutElement.php
+++ lib/MBMigration/Builder/Layout/Theme/Tradition/Elements/Sermons/MediaLayoutElement.php
@@ -1,12 +1,20 @@
 <?php
+
 namespace MBMigration\Builder\Layout\Theme\Tradition\Elements\Sermons;
+
+use MBMigration\Builder\BrizyComponent\BrizyComponent;
 
 class MediaLayoutElement extends \MBMigration\Builder\Layout\Common\Elements\Sermons\MediaLayoutElement
 {
+    protected function afterTransformToItem(BrizyComponent $brizySection): void
+    {
+        $brizySection->getValue()->set('fullHeight', 'auto');
+    }
+
     protected function getPropertiesMainSection(): array
     {
         return [
-            "mobilePaddingType"=> "ungrouped",
+            "mobilePaddingType" => "ungrouped",
             "mobilePadding" => 0,
             "mobilePaddingSuffix" => "px",
             "paddingTop" => 50,
