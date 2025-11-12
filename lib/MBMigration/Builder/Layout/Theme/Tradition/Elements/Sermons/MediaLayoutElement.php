63ec99bb839582304b4b4a1fc503c9705a6a13ae
Zaharia Alexandru <alecszaharia@gmail.com>
2025-11-12 15:08:09 +0200

    feat: enhance BrizyComponent and MediaLayoutElement functionality

--- lib/MBMigration/Builder/Layout/Theme/Tradition/Elements/Sermons/MediaLayoutElement.php
+++ lib/MBMigration/Builder/Layout/Theme/Tradition/Elements/Sermons/MediaLayoutElement.php
@@ -15,15 +15,40 @@ class MediaLayoutElement extends \MBMigration\Builder\Layout\Common\Elements\Ser
     protected function internalTransformToItem(ElementContextInterface $data): BrizyComponent
     {
         $brizySection = parent::internalTransformToItem($data);
-        $brizySectionValue = $brizySection->getItemValueWithDepth(0);
 
-        $sectionProperties = $this->getPropertiesMainSection();
-        foreach ($sectionProperties as $key => $value) {
-            $properties = 'set_' . $key;
-            $brizySectionValue->$properties($value);
+        $items = $brizySection->getItemWithDepth(0)->getValue()->get('items');
+        // create two separate sections for text and media
+        // extract text part components
+        $sectionValueProperties = $brizySection->getItemValueWithDepth(0);
+
+        $mainSection = new BrizyComponent(json_decode($this->brizyKit['GridMediaLayout']['section'], true));
+
+        $aComp = $mainSection->getItemValueWithDepth(0,0);
+        foreach ($sectionValueProperties as $key=>$value) {
+            if ($key === 'items' || strpos($key, '_') === 0 ||
+            strpos($key, 'paddingBottom') !== false  ||
+             strpos($key, 'paddingLeft') !== false ||
+             strpos($key, 'paddingRight') !== false ||
+             strpos($key, 'width') !== false ||
+             strpos($key, 'widthSuffix') !== false
+             ) {
+                continue;
+            }
+            $str = "set_$key";
+            $aComp->$str($value);
         }
 
-        return $brizySection;
+        $mainSection->getItemValueWithDepth(0,0,0,0,0)->set('items', array_slice($items, 0, -1));
+
+        $lastKey = array_key_last($items);
+        $mdElement = $items[$lastKey];
+        $mdElement->getValue()->set('mobilePaddingType', 'grouped');
+        $mdElement->getValue()->set('paddingType', 'grouped');
+        $mdElement->getValue()->set('padding', 0);
+        $mdElement->getValue()->set('mobilePadding', 0);
+        $mainSection->getItemValueWithDepth(0,1,0,0,0)->set('items', [$mdElement]);
+
+        return $mainSection;
     }
 
     protected function getPropertiesMainSection(): array
